c029fbe42b2771317c5fb2ee9b91e989c8e0275c
fromfeizhou <763457385@qq.com>
2017-10-25 18:28:54 +0800

    Mesh
    Mesh

--- Assets/Tests/TestTriangle.cs
+++ Assets/Tests/TestTriangle.cs
@@ -14,7 +14,9 @@ public class TestTriangle : MonoBehaviour
 
     public float sideLength = 2;
     public float angleDegree = 100;
-
+    private static readonly int ANGLE_DEGREE_PRECISION = 1000;
+    private static readonly int SIDE_LENGTH_PRECISION = 1000;
+    private CircleMeshCreator creator = new CircleMeshCreator();  
     private MeshFilter meshFilter;
 
     [ExecuteInEditMode]
@@ -22,12 +24,13 @@ public class TestTriangle : MonoBehaviour
     {
 
         meshFilter = GetComponent<MeshFilter>();
-        meshFilter.mesh = Create(sideLength, angleDegree);
+        meshFilter.mesh = creator.CreateMesh();
     }
 
     private void Update()
     {
-    }
+        meshFilter.mesh = creator.CreateMesh();
+    }  
     private Mesh Create(float sideLength, float angleDegree)
     {
         Mesh mesh = new Mesh();
@@ -38,8 +41,8 @@ public class TestTriangle : MonoBehaviour
         vertices[0] = Vector3.zero;
         float cosA = Mathf.Cos(halfAngle);
         float sinA = Mathf.Sin(halfAngle);
-        vertices[1] = new Vector3(cosA * sideLength, 0, sinA * sideLength);
-        vertices[2] = new Vector3(cosA * sideLength, 0, -sinA * sideLength);
+        vertices[1] = new Vector3(cosA * sideLength, sinA * sideLength, 0);
+        vertices[2] = new Vector3(cosA * sideLength, -sinA * sideLength, 0);
 
         int[] triangles = new int[3];
         triangles[0] = 0;
@@ -77,6 +80,124 @@ public class TestTriangle : MonoBehaviour
         Gizmos.DrawLine(mesh.vertices[tris[0]], mesh.vertices[tris[1]]);
         Gizmos.DrawLine(mesh.vertices[tris[0]], mesh.vertices[tris[2]]);
         Gizmos.DrawLine(mesh.vertices[tris[1]], mesh.vertices[tris[2]]);
-    }  
+    }
+
+    private class CircleMeshCreator
+    {
+        private float _radius = 5f;
+        private int _segment = 50;
+
+        private Mesh _cacheMesh;
+        public Mesh CreateMesh()
+        {
+            if (_cacheMesh == null)
+            {
+                _cacheMesh = Create();
+            }
+            return _cacheMesh;
+        }
+
+        private Mesh Create()
+        {
+            Mesh mesh = new Mesh();
+            int vlen = 1 + _segment;
+            Vector3[] vertices = new Vector3[vlen];
+            vertices[0] = Vector3.zero;  
+
+            float angleDegree = 360;
+            float angle = angleDegree * Mathf.Deg2Rad;
+            float curAngle = angle / 2;
+            float deltaAngle = angle / _segment;
+            for (int i = 1; i < vlen; i++)
+            {
+                float cosA = Mathf.Cos(curAngle);
+                float sinA = Mathf.Sin(curAngle);
+                vertices[i] = new Vector3(cosA * _radius, sinA * _radius,0);
+                curAngle -= deltaAngle;  
+            }
+
+            int tlen = _segment * 3;
+            int[] triangles = new int[tlen];
+            for (int i = 0, vi = 1; i < tlen - 3; i += 3, vi++)
+            {
+                triangles[i] = 0;
+                triangles[i + 1] = vi;
+                triangles[i + 2] = vi + 1;
+            }
+            triangles[tlen - 3] = 0;
+            triangles[tlen - 2] = vlen - 1;
+            triangles[tlen - 1] = 1;  //链接第一个点
+
+            Vector2[] uvs = new Vector2[vlen];
+            for (int i = 0; i < vlen; i++)
+            {
+                uvs[i] = new Vector2(vertices[i].x / _radius / 2 + 0.5f, vertices[i].y / _radius / 2 + 0.5f);
+            }
+
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+            mesh.uv = uvs;
+            return mesh;
+        }
+    }
+
+    private class TriangleMeshCreator
+    {
+        private float _sideLength;
+        private float _angleDegree;
+
+        private Mesh _cacheMesh;
+        public Mesh CreateMesh(float sideLength = 2, float angleDegree = 120)
+        {
+            if (checkDiff(sideLength, angleDegree))
+            {
+                Mesh newMesh = Create(sideLength, angleDegree);
+                if (newMesh != null)
+                {
+                    _cacheMesh = newMesh;
+                    this._sideLength = sideLength;
+                    this._angleDegree = angleDegree;
+                }
+            }
+            return _cacheMesh;
+        }
+
+        private Mesh Create(float sideLength, float angleDegree)
+        {
+            Mesh mesh = new Mesh();
+            Vector3[] vertices = new Vector3[3];
+
+            float angle = Mathf.Deg2Rad * angleDegree;
+            float halfAngle = angle / 2;
+            vertices[0] = Vector3.zero;
+            float cosA = Mathf.Cos(halfAngle);
+            float sinA = Mathf.Sin(halfAngle);
+            vertices[1] = new Vector3(cosA * sideLength, 0, sinA * sideLength);
+            vertices[2] = new Vector3(cosA * sideLength, 0, -sinA * sideLength);
+
+            int[] triangles = new int[3];
+            triangles[0] = 0;
+            triangles[1] = 1;
+            triangles[2] = 2;
+
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+
+            Vector2[] uvs = new Vector2[vertices.Length];
+            uvs[0] = new Vector2(0, 0.5f);
+            uvs[1] = Vector2.one;
+            uvs[2] = Vector2.right; 
+            mesh.uv = uvs;
+
+            return mesh;
+        }
+
+        private bool checkDiff(float sideLength, float angleDegree)
+        {
+            return (int)((sideLength - this._sideLength) * SIDE_LENGTH_PRECISION) != 0 ||
+                (int)((angleDegree - this._angleDegree) * ANGLE_DEGREE_PRECISION) != 0;
+        }
+
+    }
 
 }
