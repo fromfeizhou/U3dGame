5db95b5b6677a83049ad17da391799bc93789ac5
fromfeizhou <763457385@qq.com>
2017-11-17 18:03:04 +0800

    ImportModel

---
+++ Assets/Editor/Character/DisableMaterialImport.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+/************************************************************************/
+/*               fbx自动导入的时候，设置    importMaterials = false                                                    */
+/************************************************************************/
+class DisableMaterialImport : AssetPostprocessor
+{
+    void OnPreprocessModel()
+    {
+        ModelImporter modelImporter = assetImporter as ModelImporter;
+        modelImporter.importMaterials = false;
+
+    }
+}
