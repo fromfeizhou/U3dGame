e327c6ef74331d63e96cc7a2a442705ce4e50d05
fromfeizhou <763457385@qq.com>
2017-11-18 16:42:12 +0800

    Chara

--- Assets/Editor/Character/DisableMaterialImport.cs
+++ Assets/Editor/Character/DisableMaterialImport.cs
@@ -11,6 +11,29 @@ class DisableMaterialImport : AssetPostprocessor
     {
         ModelImporter modelImporter = assetImporter as ModelImporter;
         modelImporter.importMaterials = false;
+    }
 
+    void OnPostprocessModel(GameObject go)
+    {
+        if (!assetPath.Contains("/Characters/")) return;
+
+        // Assume an animation FBX has an @ in its name,
+        // to determine if an fbx is a character or an animation.
+        if (assetPath.Contains("@"))
+        {
+            // For animation FBX's all unnecessary Objects are removed.
+            // This is not required but improves clarity when browsing assets.
+
+            // Remove SkinnedMeshRenderers and their meshes.
+            foreach (SkinnedMeshRenderer smr in go.GetComponentsInChildren<SkinnedMeshRenderer>())
+            {
+                Object.DestroyImmediate(smr.sharedMesh, true);
+                Object.DestroyImmediate(smr.gameObject);
+            }
+
+            // Remove the bones.
+            foreach (Transform o in go.transform)
+                Object.DestroyImmediate(o.gameObject);
+        }
     }
 }
